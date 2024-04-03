adc8bed2c9d507982d20e2008a4188aa2c277ed6
Rahul Sharma <rahul.sharma3@publicissapient.com>
2024-04-03 20:14:20 +0530

    Moving content provider

--- apps/otc-trading-ui/src/app/app.tsx
+++ apps/otc-trading-ui/src/app/app.tsx
@@ -1,16 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ContentProvider } from './components/ContentProvider/ContentProvider';
+
 import { Header } from './components/Header';
 import { TradeForm } from './components/TradeForm';
 
 export function App() {
   return (
-    <ContentProvider>
+    <>
       <Header />
       <div className="container">
         <TradeForm />
       </div>
-    </ContentProvider>
+    </>
   );
 }
 
